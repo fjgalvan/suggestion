7b068880cfb4d64b042ce299d414aa588671e738
fjgalvan <fjgalvanx87@gmail.com>
2020-10-26 10:27:30 -0300

    main corrección final

--- suggestion/suggestionView/src/main/java/ui/MainApp.java
+++ suggestion/suggestionView/src/main/java/ui/MainApp.java
@@ -6,12 +6,12 @@ import model.SuggestionsService;
 
 public class MainApp {
 	public static void main(String[] args) {
-		SuggestionsService m = new SuggestionsService("C:\\MecanismosDeConexionExterna");
-		ViewApp v = new ViewApp();
+		SuggestionsService modelService = new SuggestionsService("C:\\MecanismosDeConexionExterna");
+		ViewApp view = new ViewApp();
 		List<String> listFactoriesName = new ArrayList<String>();
 		listFactoriesName.add("configuration.TwitterFactory");
 		listFactoriesName.add("configuration.ExcelFactory");
-		ControllerApp c = new ControllerApp(m, v);
-		c.init(listFactoriesName);
+		ControllerApp controller = new ControllerApp(modelService, view);
+		controller.init(listFactoriesName);
 	}
 }
