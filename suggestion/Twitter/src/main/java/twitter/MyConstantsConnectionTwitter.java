a6d3896e0b64c39b0c511cfd9b25c7019a55c656
fjgalvan <fjgalvanx87@gmail.com>
2021-06-30 18:59:15 -0300

    Update MyConstantsConnectionTwitter.java

--- suggestion/Twitter/src/main/java/twitter/MyConstantsConnectionTwitter.java
+++ suggestion/Twitter/src/main/java/twitter/MyConstantsConnectionTwitter.java
@@ -1,9 +1,9 @@
 package twitter;
 
 public class MyConstantsConnectionTwitter {
-	public static final String ConsumerKey = "EMdOuFV1ZYihp07HJ2fxyIRmQ";
-	public static final String ConsumerSecret = "pHT9W11iEJehJbo7OehHU1CblUmdR7d1PB7aN3XFhP2013x6YU";
-	public static final String AccessToken = "1039948114180878337-KYnV5SpAyyd2YZ0MKlL8KpFuuOnud7";
-	public static final String AccessTokenSecret = "JH6vcsL06QpcuKosjNWPIVScdsOXp9Ct7cH8Sg6605jab";
+	public static final String ConsumerKey = "ConsumerKey";
+	public static final String ConsumerSecret = "ConsumerSecret";
+	public static final String AccessToken = "AccessToken";
+	public static final String AccessTokenSecret = "AccessTokenSecret";
 }
 
